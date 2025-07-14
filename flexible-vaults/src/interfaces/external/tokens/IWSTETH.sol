cb24b204269172c9c4121c77dcbf2294cbbf8b72
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2025-07-14 16:48:17 +0200

    Delete scope directories

--- flexible-vaults/src/interfaces/external/tokens/IWSTETH.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.25;
-
-interface IWSTETH {
-    function stETH() external view returns (address);
-
-    function wrap(uint256 _stETHAmount) external returns (uint256);
-    function unwrap(uint256 _wstETHAmount) external returns (uint256);
-}
